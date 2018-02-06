e4863de02f6cc4f519c4be3170c8a9f2510ccca9
obarquero <oscar.barquero@urjc.es>
2018-02-06 15:44:05 +0100

    Patient class
    Patient class

---
+++ pat_class.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 17 12:02:46 2016
+
+@author: Rebeca Goya Esteban
+         Óscar Barquero Pérez
+         Luaura Ruano
+"""
+
+class pat(Object):
+    """
+    Class that model a patient.
+    """
+
+    def __init__(self,ident):
+        
+        self.id = ident
